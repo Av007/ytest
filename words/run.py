3ca883e5de6d0e67d77351b5c01b4c3a5fc917c6
Av007 <avdeevvladimir@gmail.com>
2024-12-14 11:56:45 +0200

    adding if main for running

--- words/run.py
+++ words/run.py
@@ -2,5 +2,6 @@ from src import is_word
 
 valid_words = {'pop', 'rom', 'corn', 'popcorn', 'rock', 'mock', 'ok'}
 
-words = is_word(valid_words)
-assert len(words) == len(valid_words)
+if __name__ == '__main__':
+    words = is_word(valid_words)
+    assert len(words) == len(valid_words)
